6922c96dd93510a5ba263bafe92321871bc070fe
Akikazu Andika <akikazuandika@gmail.com>
2018-11-04 23:30:52 +0700

    Init

---
+++ application/views/project_about.php
@@ -0,0 +1,19 @@
+<div class="container">
+        <h2 class="text-center text-uppercase text-white">Muqoddimah</h2>
+        <hr class="star-light mb-5">
+        <div class="row">
+          <div class="col-lg-4 ml-auto">
+            <p class="lead">Ilmu adalah sebuah jalan yang bisa memahami isi kehidupan. Karna menurut hadist Riwayat Turmudzi. dikatakan bahwa barang siapa yang menginginkan dunia makan dengan ilmu, Barang siapa yang menginginkan Akhirat maka dengan Ilmu, Dan Barang siapa menginginkan keduanya maka dengan Ilmu. sehingga Orang tanpa ilmu bagaikan orang yang membawa pedang tetapi ia tidak tau untuk apa ia digunakan.</p>
+          </div>
+          <div class="col-lg-4 mr-auto">
+            <p class="lead">Sedangkan Pendidikan yaitu Sarana Untuk tercapainya suatu kengininan yang di landasi oleh hati kita, pendidikan bukanlah materi melainkan nutrisi bagi jiwa yang haus akan ilmu, Dan Pendidikan mampu melahirkan orang orang optimis, karna sebutir optimisme lebih berharga dari pada sekarung bakat terpendam</p>
+          </div>
+        </div>
+        <div class="text-center mt-4">
+          <a class="btn btn-xl btn-outline-light" href="#">
+            
+            Back!
+          </a>
+        </div>
+      </div>
+      
---
+++ application/views/project_sb.php
@@ -0,0 +1,44 @@
+<div class="container">
+        <h2 class="text-center text-uppercase text-secondary mb-0">Menu</h2>
+        <hr class="star-dark mb-5">
+        <div class="row">
+          <div class="col-md-6 col-lg-4">
+            <a class="portfolio-item d-block mx-auto" href=index.php/menu_1.php>
+              <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                  <i class="fas fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="fix/img/fix2/buku.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-2">
+              <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                  <i class="fas fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="fix/img/fix2/quiz.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-3">
+              <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                  <i class="fas fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="fix/img/fix2/dis.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-4">
+              
+              
+            </a>
+          </div>
+          
+        </div>
+      </div>
+    
