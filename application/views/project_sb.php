3a95bc9f97f499bf26f352a5f6a49d0134c3481b
userman99 <febriankusuma104@gmail.com>
2018-11-14 22:04:49 +0700

    tambahan link untuk tiap isi dalam menu

--- application/views/project_sb.php
+++ application/views/project_sb.php
@@ -3,7 +3,7 @@
         <hr class="star-dark mb-5">
         <div class="row">
           <div class="col-md-6 col-lg-4">
-            <a class="portfolio-item d-block mx-auto" href=index.php/menu_1.php>
+            <a class="portfolio-item d-block mx-auto" href=index.php/view_menu>
               <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
                 <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
                   <i class="fas fa-search-plus fa-3x"></i>
@@ -11,9 +11,12 @@
               </div>
               <img class="img-fluid" src="fix/img/fix2/buku.jpg" alt="">
             </a>
+            <div class>
+            <p class="lead" align="center" >   Materi Rasmul Bayan</p>
+          </div>
           </div>
           <div class="col-md-6 col-lg-4">
-            <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-2">
+            <a class="portfolio-item d-block mx-auto" href="index.php/view_menu2">
               <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
                 <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
                   <i class="fas fa-search-plus fa-3x"></i>
@@ -21,9 +24,12 @@
               </div>
               <img class="img-fluid" src="fix/img/fix2/quiz.jpg" alt="">
             </a>
+            <div class>
+            <p class="lead" align="center" >   Quizz</p>
+          </div>
           </div>
           <div class="col-md-6 col-lg-4">
-            <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-3">
+            <a class="portfolio-item d-block mx-auto" href="index.php/view_menu3">
               <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
                 <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
                   <i class="fas fa-search-plus fa-3x"></i>
@@ -31,6 +37,9 @@
               </div>
               <img class="img-fluid" src="fix/img/fix2/dis.jpg" alt="">
             </a>
+            <div class>
+            <p class="lead" align="center" >   Diskusi atau Pertanyaan</p>
+          </div>
           </div>
           <div class="col-md-6 col-lg-4">
             <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-4">
