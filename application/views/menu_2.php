83d26f9dc7da2cbf9bcb5b7296e4af9962fd665c
userman99 <febriankusuma104@gmail.com>
2018-12-12 21:15:43 +0700

    menu dari quiz

--- application/views/menu_2.php
+++ application/views/menu_2.php
@@ -7,13 +7,192 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
    
 
-    <title>quiz</title>
+    <title>Ujian</title>
 
   </head>
 
 
 <body>
 
-  <h3> TES 2 </h3>
+  <h3> <html>
+<head>
+    <title>SOAL PILIHAN GANDA</title>
+</head>
+<body bgcolor="white">
+<h2>
+<center>
+<align = center>TARBIYAH RASMUL BAYAN<br>
+<br>
+UJIAN DASAR TARBIYAH<br>
+TAHUN AKADEMIK 2018/2019<br>
+</center>
+<hr>
+<center>
+<b>
+<table width="320" border="0">
+<tr>
+    <td width="186"><b>Mata Kuliah</b></td>
+    <td width="186"><b>: tarbiyah</b></td>
+  </tr>
+<tr>
+    <td><b>Semester</b></td>
+    <td><b>: I (Satu)</b></td>
+  </tr>
+<tr>
+    <td><b>Waktu</b></td>
+    <td><b>: 90 menit</b></td>
+  </tr>
+<tr>
+    <td><b>Sifat</b></td>
+    <td><b>: Close Book</b></td>
+  </tr>
+</table>
+</b>
+</center>
+<br>
+<hr>
+<br>
+
+<p>
+<u><h2>
+SOAL PILIHAN GANDA</h2>
+</U></P>
+<form id="form1" name="form1" method="post" action="">
+ <ol start="1" type="1">
+<li>Dintara berikut mana saja yang termasuk dalam BAB Makna Syahadat</li>
+<br>
+    
+   
+   <ol start="1" type="a">
+    <i>
+<li><input type="radio" name="NO1" value="A">kandungan Syhadat</li>
+<li><input type="radio" name="NO1" value="B">Makna illah</li>
+<li><input type="radio" name="NO1" value="C">pemurnian ibadah</li>
+<li><input type="radio" name="NO1" value="D">Ridha</li>
+</i>  
+   </ol>
+<br>
+<li>diantara sifat sifat Rosul adalah, <i>kecuali</i></li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO2" value="A">mengeluh</li>
+<li><input type="radio" name="NO2" value="B">terpercaya</li>
+<li><input type="radio" name="NO2" value="C">cerdas</li>
+<li><input type="radio" name="NO2" value="D">amanah</li>
+</i>
+   </ol>
+<br>
+<li>Di Antara berikut tugas Rosul adalah</li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO3" value="a">mencari nafkah</li>
+<li><input type="radio" name="NO3" value="b">Meneruskan risalah</li>
+<li><input type="radio" name="NO3" value="c">diam sama</li>
+<li><input type="radio" name="NO3" value="d">mengusung risalah dakwah</li>
+
+</i>
+   </ol>
+<br>
+<li>salah satu jalan untuk mengenal alloh adalah</li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO4" value="a">menyepi dengan ritual syirik</li>
+<li><input type="radio" name="NO4" value="b">mempelajari firman alloh</li>
+<li><input type="radio" name="NO4" value="c">berdiam diri saja</li>
+<li><input type="radio" name="NO4" value="d">menghafal primbun jawa</li>
+</i>
+   </ol>
+<br>
+<li>sebutkan 2 Tugas Rosul yang paling utama</li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO5" value="a"> Berdiam dan Bersantai</li>
+<li><input type="radio" name="NO5" value="b">Mengemban risalah dan menyempurnakan ahlak</li>
+<li><input type="radio" name="NO5" value="c">Mencintai dan dicintai sesama mahluk</li>
+<li><input type="radio" name="NO5" value="d">Bergurau dan Cari sensasi</li>
+</i>
+   </ol>
+<br>
+<li>Salah Satu Ciri Ciri dari Islam Sebagai pedoman Hidup adalah <i>kecuali</i></li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO6" value="a">Keyakinan</li>
+<li><input type="radio" name="NO6" value="b">Tingkah Laku</li>
+<li><input type="radio" name="NO6" value="c">Pndidkan</li>
+<li><input type="radio" name="NO6" value="d">Bersantai</li>
+</i>
+   </ol>
+<br>
+<li>Bukti Keberadaan Alloh Adalah</li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO7" value="a">Bukti Sejarah</li>
+<li><input type="radio" name="NO7" value="b">Adanya Teknologi</li>
+<li><input type="radio" name="NO7" value="c">Menyepi</li>
+<li><input type="radio" name="NO7" value="d">Puasa senin kamis</li>
+
+</i>
+   </ol>
+<br>
+<li>Hasil Dari kita mengikuti rosul adalah Di Dunia <i> Kecuali</i></li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO8" value="a"> Di CIntai Alloh</li>
+<li><input type="radio" name="NO8" value="b">Dirahmati alloh</li>
+<li><input type="radio" name="NO8" value="c">Di beri petnjuk alloh</li>
+<li><input type="radio" name="NO8" value="d">Mendapatkan uang</li>
+</i>
+   </ol>
+<br>
+<li>Diantara berikut mana di antaranya dari penyakit ragu, <i>kecuali</i></li>
+<br>
+ <ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO9" value="a">Bodoh </li>
+<li><input type="radio" name="NO9" value="b">Ragu-Ragu</li>
+<li><input type="radio" name="NO9" value="c">Lalai</li>
+<li><input type="radio" name="NO9" value="d">Pendiam</li>
+</i>
+   </ol>
+<br>
+<li>Perhatikan beberapa sifat-sifat system operasi dibawah ini :</li>
+<br>
+1. pendosa <br>
+2. Tawakal<br>
+3. Sombong <br>
+4. Dusta <br>
+5. Qonaah <br>
+<br>  
+Dari beberapa pernyataan manakah diantara penyakit syahwat </p>
+</ol>
+     
+<ol start="1" type="a">
+     <i>
+<li><input type="radio" name="NO10" value="a">1, 2, 3</li>
+<li><input type="radio" name="NO10" value="b">2, 3 ,5</li>
+<li><input type="radio" name="NO10" value="c">1, 3, 5</li>
+<li><input type="radio" name="NO10" value="d">1, 4, 5</li>
+<li><input type="radio" name="NO10" value="e">1, 4, 2</li>
+</i>
+   </ol>
+</ol>
+</form>
+<br>
+ <marquee behavior="alternate" scrollamount="5"> <font name="Forte"><u><h3>
+SELAMAT MENGERJAKAN</h2>
+</u></font> <marquee>  
+        
+</body>
+</html>
+
+
+ </h3>
       </body>
       </html>
